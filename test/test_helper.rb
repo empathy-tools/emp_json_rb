d14ea4a7a286cfb5a962b1cc9e38b12505a71f07
Arthur Dingemans <arthur@argu.co>
2022-05-30 15:35:32 +0200

    Fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
 require "empathy/emp_json"
 
 require "minitest/autorun"
